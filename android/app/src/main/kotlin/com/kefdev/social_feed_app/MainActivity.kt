e5106cdd6cbb1a904de0e69a0f08218b2704310e
Kevin Gordillo <kevin.gdl.131@gmail.com>
2026-04-15 08:40:06 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/kefdev/social_feed_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kefdev.social_feed_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _PlaceholderApp());
+}
+
+class _PlaceholderApp extends StatelessWidget {
+  const _PlaceholderApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('SocialFeedApp - scaffold only'),
+        ),
+      ),
+    );
+  }
+}
